829fd751942fb271a600e1953d50f74fa2b190cd
devgyuri <gksmf910@gmail.com>
2024-01-13 22:58:09 +0900

    adding pagination

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -4,12 +4,11 @@ export const globalStyles = css`
   * {
     margin: 0;
     box-sizing: border-box;
-    color: purple;
     font-family: "notoFont";
   }
 
   @font-face {
     font-family: "notoFont";
-    src: url("/fonts/Noto_Sans_KR/static/NotoSansKR-ExtraBold.ttf");
+    src: url("/fonts/Noto_Sans_KR/static/NotoSansKR-Regular.ttf");
   }
 `;
