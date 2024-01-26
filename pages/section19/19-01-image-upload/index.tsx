6b580773e048c8a1810b9ca9699a5d7eadc37b10
devgyuri <gksmf910@gmail.com>
2024-01-26 15:12:52 +0900

    image upload

---
+++ pages/section19/19-01-image-upload/index.tsx
@@ -0,0 +1,46 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, type ChangeEvent } from "react";
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    const file = event.target.files?.[0];
+    console.log(file);
+
+    const result = await uploadFile({
+      variables: { file },
+    });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onChangeFile} />
+      {
+        imageUrl !== "" &&
+        <img src={`https://storage.googleapis.com/${imageUrl}`} />
+      }
+    </>
+  );
+}
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,13 +1,17 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client"
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+  const uploadLink: ApolloLink = createUploadLink({
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql"
+  });
+
   const client = new ApolloClient({
-    // uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+    link: ApolloLink.from([uploadLink]),
     cache: new InMemoryCache(),
   });
 
