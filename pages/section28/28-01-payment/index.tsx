84dd1c7f5aa5f38d32566c20ac7a6223d167909d
devgyuri <gksmf910@gmail.com>
2024-02-11 17:57:29 +0900

    payment test2

--- pages/section28/28-01-payment/index.tsx
+++ pages/section28/28-01-payment/index.tsx
@@ -1,5 +1,33 @@
+declare const window: typeof globalThis & {
+  IMP: any;
+};
+
 export default function PaymentPage(): JSX.Element {
-  const onClickPayment = (): void => {};
+  const onClickPayment = (): void => {
+    const IMP = window.IMP;
+    IMP.init("imp49910675");
+
+    IMP.request_pay(
+      {
+        pg: "kakaopay",
+        pay_method: "card",
+        name: "mouse",
+        amout: 10,
+        buyer_email: "abc@h.com",
+        buyer_name: "kitty",
+        buyer_tel: "010-1234-5678",
+        buyer_addr: "서울특별시 강남구",
+        buyer_postcode: "12345",
+      },
+      (rsp: any) => {
+        if (rsp.success === true) {
+          console.log("success");
+        } else {
+          console.log("fail");
+        }
+      },
+    );
+  };
 
   return (
     <>
