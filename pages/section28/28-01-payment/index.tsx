aa20d78687165fc045c18d18dc64f2a464f8e147
devgyuri <gksmf910@gmail.com>
2024-02-12 22:54:57 +0900

    payment

--- pages/section28/28-01-payment/index.tsx
+++ pages/section28/28-01-payment/index.tsx
@@ -5,23 +5,27 @@ declare const window: typeof globalThis & {
 export default function PaymentPage(): JSX.Element {
   const onClickPayment = (): void => {
     const IMP = window.IMP;
-    IMP.init("imp49910675");
+    IMP.init("imp12518572");
 
     IMP.request_pay(
       {
         pg: "kakaopay",
         pay_method: "card",
+        // merchant_uid: "ORD20180131-0000011",
         name: "mouse",
-        amout: 10,
+        amount: 10,
         buyer_email: "abc@h.com",
         buyer_name: "kitty",
         buyer_tel: "010-1234-5678",
         buyer_addr: "서울특별시 강남구",
         buyer_postcode: "12345",
+        m_redirect_url: "http://localhost:3000/section28/28-01-payment",
       },
       (rsp: any) => {
         if (rsp.success === true) {
           console.log("success");
+          console.log(rsp);
+          // createPointTransactionOfLoading();
         } else {
           console.log("fail");
         }
